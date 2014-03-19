b17a55fe18c545950fb3578a08a8b99397605105
luc sinet <luc.sinet@epitech.eu>
2014-03-19 10:12:06 +0100

    forgot file

--- src/Includes/IGraphics.hpp
+++ src/Includes/IGraphics.hpp
@@ -6,10 +6,10 @@ class IGraphics
 public:
   virtual ~IGraphics() {};
 
-  virtual void create_windows(const std::string &name) = 0;
+  virtual void create_window(const std::string &name) = 0;
   virtual void clear() = 0;
   virtual void draw(const std::string &map) = 0;
-  virtual void destroyWindows();
+  virtual void destroyWindow();
 };
 
 #endif /* _IGRAPHICS_H_ */
