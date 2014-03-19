0dfd1685511e9e1c957e4938ffe87ae1b7dd5380
luc sinet <luc.sinet@epitech.eu>
2014-03-19 10:11:43 +0100

    window not windowS

---
+++ src/Includes/Graphics.hpp
@@ -0,0 +1,16 @@
+#ifndef _GRAPHICS_H_
+#define _GRAPHICS_H_
+
+class Graphics : IGraphics
+{
+public:
+  Graphics();
+  virtual ~Graphics();
+
+  virtual void create_window(const std::string &name);
+  virtual void clear();
+  virtual void draw(const std::string &map);
+  virtual void destroyWindow();
+};
+
+#endif /* _GRAPHICS_H_ */
