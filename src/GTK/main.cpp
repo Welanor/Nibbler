e0fff559974e095e7f5ee28cbd8061fd978503a9
luc sinet <luc.sinet@epitech.eu>
2014-03-19 09:56:49 +0100

    moved hpp in include folder and edited makefiles

--- src/GTK/main.cpp
+++ src/GTK/main.cpp
@@ -1,4 +1,10 @@
-int	main()
+int		main()
 {
-  return (0);
+  /*  GtkWidget	*window;
+
+  gtk_init(NULL, NULL);
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_widget_show(window);
+  gtk_main();
+  return (0);*/
 }
