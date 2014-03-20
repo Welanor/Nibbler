250a6eacef90f3f2b89b77a8d2448ad3efeebabc
luc sinet <luc.sinet@epitech.eu>
2014-03-20 15:31:22 +0100

    graphics.hpp

---
+++ src/GTK/Graphics.hpp
@@ -0,0 +1,23 @@
+#ifndef _GRAPHICS_H_
+# define _GRAPHICS_H_
+
+# include <gtk/gtk.h>
+# include "IGraphics.hpp"
+
+class Graphics : public IGraphics
+{
+public:
+  Graphics();
+  virtual ~Graphics();
+
+  virtual void create_window(const std::string &name, const int, const int);
+  virtual void init_events(bool *key);
+  virtual void clear();
+  virtual void draw(const std::list<t_snake> &snake);
+  virtual void destroyWindow();
+
+private:
+  GtkWidget	*_window;
+};
+
+#endif /* _GRAPHICS_H_ */
